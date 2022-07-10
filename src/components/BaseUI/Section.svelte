1fa9a055314f3daa1805a57da04d45745a74ec3e
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-07-10 13:11:42 +0200

    Move section styles to Section component

--- src/components/BaseUI/Section.svelte
+++ src/components/BaseUI/Section.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
 	export let title = 'Section';
 	export let sectionStatus: Status = null;
-	let error = sectionStatus === "error";
-	let success = sectionStatus === "success";
+	let error = sectionStatus === 'error';
+	let success = sectionStatus === 'success';
 </script>
 
 <section class:error class:success>
 	<header>
-		{#if sectionStatus === "success"}
+		{#if sectionStatus === 'success'}
 			<span class="material-symbols-outlined icon-check"> check_circle </span>
-		{:else if sectionStatus === "error"}
+		{:else if sectionStatus === 'error'}
 			<span class="material-symbols-outlined icon-error"> error </span>
 		{/if}
 		<h2>{title}</h2>
@@ -18,15 +18,21 @@
 </section>
 
 <style lang="scss">
+	section {
+		padding: 24px 0 0;
+		margin-bottom: 24px;
+		border-top: 1px solid colors.$black-a12;
+	}
 	header {
 		display: flex;
 		h2 {
 			width: calc(100% - 24px - 8px);
 		}
 	}
-    .icon-check, .icon-error {
-        margin-right: 8px;
-    }
+	.icon-check,
+	.icon-error {
+		margin-right: 8px;
+	}
 	.icon-check {
 		color: colors.$success;
 	}
