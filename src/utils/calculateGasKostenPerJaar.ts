f91aa25543439439ef06c2e6208d9e054173f56a
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-07-10 17:33:15 +0200

    Pull caculations for gas to separate file

---
+++ src/utils/calculateGasKostenPerJaar.ts
@@ -0,0 +1,20 @@
+const calculateGasKostenPerJaar = (
+    gasConsumptie: number | null,
+	gasTotVarKosten: number | null,
+	gasVasteLevKosten: number | null,
+	gasNetbeheerKosten: number | null
+) => {
+    /*===== GAS ===== */
+    const isAllDataFilled = gasConsumptie && gasTotVarKosten && gasVasteLevKosten && gasNetbeheerKosten;
+    if (!isAllDataFilled) return null;
+    const gasTotVarKostenPerJaar = gasTotVarKosten * gasConsumptie;
+    const gasVasteLevKostenPerJaar = gasVasteLevKosten * 12;
+    const gasNetbeheerKostenPerJaar = gasNetbeheerKosten * 365;
+    const gasKostenPerJaar = 
+        gasTotVarKostenPerJaar + 
+        gasVasteLevKostenPerJaar + 
+        gasNetbeheerKostenPerJaar;
+    return gasKostenPerJaar;
+}
+
+export default calculateGasKostenPerJaar;
