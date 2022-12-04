e64f1cee103177109495327c5d9d612f9bc59164
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-12-04 10:46:55 +0100

    Reinstall svelte

---
+++ src/App.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import "./scss/styles.scss";
+  import ElectricityCard from "./components/Electricity/ElectricityCard.svelte";
+  import GasCard from "./components/Gas/GasCard.svelte";
+  import Total from "./components/Total/Total.svelte";
+  import Nav from "./components/Nav.svelte";
+  import HeroShot from "./components/HeroShot.svelte";
+  import DeveloperBar from "./components/DeveloperBar/DeveloperBar.svelte";
+  let hasGas = true;
+  const buttonList = [
+    {
+      name: "Math behind calculator",
+      href: "https://github.com/sergperep/bonfire/blob/main/docs/math.md",
+      hasIcon: true,
+    },
+    {
+      name: "GitHub",
+      href: "https://github.com/sergperep/bonfire",
+      hasIcon: true,
+    },
+  ];
+</script>
+
+<Nav {buttonList} />
+<DeveloperBar />
+<main>
+  <HeroShot {buttonList} />
+  <div class="main-container">
+    <div class="content-wrapper">
+      <ElectricityCard />
+      <GasCard bind:hasGas />
+    </div>
+    <div class="total-wrapper"><Total {hasGas} /></div>
+  </div>
+</main>
+
+<style lang="scss">
+  .content-wrapper {
+    width: calc(100% - 350px - 24px);
+    @include media("<desktop") {
+      & {
+        width: 100%;
+      }
+    }
+    @include media("<tablet") {
+      & {
+        width: 100%;
+      }
+    }
+  }
+  .total-wrapper {
+    width: 350px;
+    @include media("<desktop") {
+      & {
+        width: 100%;
+        margin-bottom: 56px;
+      }
+    }
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
-
+import sveltePreprocess from 'svelte-preprocess';
 const sassColors = `@use './src/scss/sass-utils/colors.scss';`;
 const sassFonts = `@use './src/scss/sass-utils/fonts.scss';`;
 const sassVendors = `@use './src/scss/sass-utils/vendors.scss' as *;`;
 const sassMedia = `@use './src/scss/sass-utils/include-media.scss' as *;`;
 const sassString = sassColors + sassFonts + sassVendors + sassMedia;
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess({
 		scss:{
 			prependData: sassString
 		}
-	}),
-
-	kit: {
-		adapter: adapter()
-	}
-};
-
-export default config;
+	})
+}
